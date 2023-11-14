943c0b102c27ae24e2ec8c7e696ae92c82a98205
alimohammad0816 <alimohammad0816@gmail.com>
2023-11-14 19:02:36 +0330

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-zarinpal',
+    version='0.1.0',
+    author='alimohammad0816',
+    author_email='alimohammad0816@gmail.com',
+    description='python library for zarin pal rest apis',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
