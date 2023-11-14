7243fe3ce144d918d7ca84337f08cd1cdcdd0958
alimohammad0816 <alimohammad0816@gmail.com>
2023-11-15 01:07:41 +0330

    modifying version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements():
 
 setup(
     name='python-zarinpal',
-    version='0.1.1',
+    version='0.1.2',
     author='alimohammad0816',
     author_email='alimohammad0816@gmail.com',
     description='python library for zarin pal rest apis',
